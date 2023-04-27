7ec34998e20a5d0a852d3902c47dd7bd682f9400
Luis <luislybe@gmail.com>
2023-04-27 17:51:02 -0600

    Encriptado finalizado y estilo de CSS definido

--- prueba.js
+++ prueba.js
@@ -3,6 +3,12 @@ let salida = document.querySelector("#salida");
 let encriptador = document.querySelector("#encriptar");
 let desencriptador = document.querySelector("#desencriptar");
 let limpiar = document.querySelector("#limpiar");
+let copiar = document.querySelector("#copiar");
+let pegar = document.querySelector('#pegar')
+let muñeco = document.getElementById("muñeco");
+let fondo = document.getElementById("fondo");
+let diamante = document.getElementById("diamante");
+let msj = document.getElementById("mensaje");
 let op;
 
 function check() {
@@ -56,26 +62,65 @@ function check() {
       salida.value = frase;
       break;
     default:
-      salida.value = "INGRESE UNA OPCIÓN VÁLIDA";
+      muñeco.style.display = "block";
+      msj.style.display = "block";
       break;
   }
 }
 
 function cifrar() {
-    op = 0;
-    check();
+  op = 0;
+  check();
+  muñeco.style.display = "none";
+  fondo.style.display = "none";
+  diamante.style.display = "none";
+  msj.style.display = "none";
+
+  if (entrada.value == "") {
+    muñeco.style.display = "block";
+    fondo.style.display = "block";
+    diamante.style.display = "block";
+    msj.style.display = "block";
+  }
 }
 
 function decifrar() {
-    op = 1;
-    check();
+  op = 1;
+  check();
+  muñeco.style.display = "none";
+  fondo.style.display = "none";
+  diamante.style.display = "none";
+  msj.style.display = "none";
+
+  if (entrada.value == "") {
+    muñeco.style.display = "block";
+    fondo.style.display = "block";
+    diamante.style.display = "block";
+    msj.style.display = "block";
+  }
 }
 
 function borrar() {
   salida.value = "";
   entrada.value = "";
+  muñeco.style.display = "block";
+  fondo.style.display = "block";
+  diamante.style.display = "block";
+  msj.style.display = "block";
+}
+
+function copiarTexto() {
+  if (entrada.value != "") {
+    navigator.clipboard.writeText(salida.value);
+    alert("Texto copiado")
+  }
+}
+
+function pegarTexto(){
+  
 }
 
 encriptador.onclick = cifrar;
 desencriptador.onclick = decifrar;
 limpiar.onclick = borrar;
+copiar.onclick = copiarTexto; //Agregar función que copie el contenido al portapapeles
